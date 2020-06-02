14e0e6da8c0c2dada04c39506226fc7815c304ab
CyberAlv <munaralvin@gmail.com>
2020-06-02 19:59:38 -0400

    Added lines of code

---
+++ src/ContactCard.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class ContactCard extends Component {
+    render() {
+        return ( 
+        <div>
+            <h1>{this.props.name}</h1>;
+            <ul>
+                <li>Phone: {this.props.phone}</li>
+                <li>Email: {this.props.email}</li>
+                <li>Address: {this.props.address}</li>
+
+            </ul>
+        </div>
+        );
+    }
+}
+
+export default ContactCard;
